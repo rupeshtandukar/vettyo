54eb1df397ef2ff3f2f0b40902b02630785dffdd
Rojan116 <rojandhimal1@gmail.com>
2020-12-14 16:07:22 +0545

    adding project

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Card = () =>{
+				return(
+				<div className='cards'>
+					<img alt='robo-img' src='' />
+					<div>
+						<h2>name</h2>
+						<p>email</p>
+					</div>
+
+				</div>);
+
+}
+
+export default Card;
